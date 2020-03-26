52527a430f0762898521967788cb302d498ed1ce
Kemunto Maragia <kmaragia@email.wm.edu>
2020-03-26 18:34:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,1 +1,25 @@
+def addition(a,b)
+  return a + b 
+end
 
+def subtraction(a,b)
+  return a - b 
+end
+
+def division(a,b)
+  return a / b 
+end
+
+def multiplication(a,b)
+  return a * b 
+end
+
+def modulo(a,b)
+  return a % b 
+end
+
+def square_root(a)
+  return Math.sqrt(a)
+end
+
+def order_of_operations
